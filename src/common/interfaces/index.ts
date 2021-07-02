70341d8052d9436a57ab50dced82f329c2312d09
NonStopTreasure <demlev572@gmail.com>
2021-07-02 03:33:27 +0300

    - Final (3:33 02.07.2021) version of Chuck Norris Page

--- src/common/interfaces/index.ts
+++ src/common/interfaces/index.ts
@@ -1,3 +1,13 @@
 export interface IChuckNorrisProps {
   key?: string
 }
+
+export interface IChuckNorrisRandom {
+  categories: string[]
+  created_at: string
+  icon_url: string
+  id: string
+  update_at: string
+  url: string
+  value: string
+}
